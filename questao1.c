8b8c89ed7048b78ae0e22cc529d7ca74e31c50d7
Djalmadh <43158721+Djalmadh@users.noreply.github.com>
2018-09-10 23:09:45 -0300

    Add files via upload

---
+++ questao1.c
@@ -0,0 +1,2 @@
+O uso de ponteiros é necessário para a elaboração de codigos de alto nivel que seriam impossiveis de elaborar
+sem seu uso, uma vez que, o referenciamento torna mais simples o desenvolvimento de codigos.
